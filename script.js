79047640a2a4248e369a9d8c9f2dc36a2b105c76
SilasPuma <114674696+SilasPuma@users.noreply.github.com>
2023-11-14 09:42:38 -0600

    Update script.js

--- script.js
+++ script.js
@@ -2,4 +2,21 @@ document.addEventListener('DOMContentLoaded', function() {
     const container = document.querySelector('.container');
     container.style.opacity = '1';
     container.style.transform = 'translateY(0)';
+
+    // Smooth scroll for navigation links
+    const navLinks = document.querySelectorAll('nav a');
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+
+            const targetId = this.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+
+            window.scrollTo({
+                top: targetElement.offsetTop - header.offsetHeight,
+                behavior: 'smooth'
+            });
+        });
+    });
 });
