01fa8abe17f8f274323b765710e210dc11ce998d
SilasPuma <114674696+SilasPuma@users.noreply.github.com>
2023-11-14 09:39:46 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const container = document.querySelector('.container');
+    container.style.opacity = '1';
+    container.style.transform = 'translateY(0)';
+});
